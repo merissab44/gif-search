fd91f54508b61e1820ffc8715bc106de416172e0
Merissa Bridgeman <merissa.bridgeman98@gmail.com>
2021-09-16 19:39:21 -0700

    added middleware

--- app.js
+++ app.js
@@ -6,11 +6,26 @@ const app = express();
 
 //Setup middleware
 
+const exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 //Setup routes
 
 app.get('/', (req, res) => {
-    res.send('Hello Squirrel')
+
+    //get the url of the gif
+    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
+    //render the gif view
+    res.render('hello-gif', { gifUrl })
 });
+
+app.get('/greetings/:name', (req,res) => {
+    //get the name
+    const name = req.params.name;
+    //render the greetings view along with the name
+    res.render('greetings', { name });
+})
 //Start the server
 
 app.listen(3000, () => {
